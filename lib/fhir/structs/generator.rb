595e05b1bd79148d843afdefc11a039d3eff2927
Alberto Colón Viera <aacv@albertico.dev>
2023-02-23 01:46:23 -0500

    Initial commit

---
+++ lib/fhir/structs/generator.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "generator/version"
+
+module Fhir
+  module Structs
+    module Generator
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
